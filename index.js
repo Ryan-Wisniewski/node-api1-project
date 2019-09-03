0afb60ba2f7a17f8cc34b8096a99b33c88174449
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2019-09-02 20:08:55 -0400

    not getting it but I will do this until it makes sense to me. :)

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // implement your API here
+const express = require('express')
+const db = require('./data/db')
+// console.log(db)
+// console.log(db.find())
+const port = 5000
+
+const server = express()
+server.use(express.json())
+
+server.post('/api/users', (req, res) => {
+    res.status(201).json({url: '/api/users', operation: 'POST'})
+})
+
+server.get('/api/users', (req, res) => {
+    
+    res.send(db.find())
+})
+
+server.get('/api/users/:id', (req, res) => {
+    res.send('eachUser')
+})
+
+server.put('/api/users/:id', (req, res) => {
+    res.status(200).json({url: '/api/users/:id', operation: 'PUT'})
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    res.sendStatus(204)
+})
+
+server.listen(port, () => {
+    console.log(`Server listening on ${port}`)
+})
